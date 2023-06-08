876b6146afd6f9c90a8c6b487c491eacca9f63b2
Reda Hammada <hammada.reda.dev@gmail.com>
2023-06-08 03:44:11 +0100

    sanctum auth package has been added
    migrations and relationships has been set
    
    main controllers API have been created

---
+++ app/Models/Preference.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use App\Models\User;
+
+class Preference extends Model
+{
+    use HasFactory;
+
+   public function User(){
+        $this->belongsTo(User::class);
+   }
+}
---
+++ database/migrations/2023_06_08_021251_create_preferences_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('preferences', function (Blueprint $table) {
+            $table->id();
+            $table->string('preferred_source');
+            $table->string('preferred_category');
+            $table->string('preferred_author');
+            $table->unsignedBigInteger('user_id');
+            $table->timestamps();
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('preferences');
+    }
+};
